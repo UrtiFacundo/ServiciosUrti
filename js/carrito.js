b784e4750671fd627f54cf31936e442235fd8dd1
Facundo Urti <facuurti@gmail.com>
2023-08-11 21:53:14 -0300

    se creo una array para los servicios y que todos los cards que mostraba la pagina de servicios a traves del html ahora se realizaron con js. Se puede contabilizar y guardar a traves de JSON, cuantas servicios se quieren contratar.

---
+++ js/carrito.js
@@ -0,0 +1,8 @@
+const productosEnCarrito = JSON.parse(localStorage.getItem("servicios-en-carrito"));
+
+const carritoVacio = document.querySelector("#carrito-vacio")
+if (serviciosEnCarrito) {
+
+} else {
+
+}
--- js/main.js
+++ js/main.js
@@ -1,43 +1,91 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const comprar1 = document.getElementById('comprar1');
-    comprar1.addEventListener('click', function () {
-      const lista = document.getElementById('lista');
-      const nuevoElemento = document.createElement('li');
-      nuevoElemento.textContent = 'Branding';
-      lista.appendChild(nuevoElemento);
-    });
-    localStorage.setItem("servicio", "Branding");
-  });
-  document.addEventListener('DOMContentLoaded', function () {
-    const comprar2 = document.getElementById('comprar2');
-    comprar2.addEventListener('click', function () {
-      const lista = document.getElementById('lista');
-      const nuevoElemento = document.createElement('li');
-      nuevoElemento.textContent = 'Publicidad';
-      lista.appendChild(nuevoElemento);
-    });
-  });
-  localStorage.setItem("servicio", "Publicidad");
-  
-document.addEventListener('DOMContentLoaded', function () {
- const comprar3 = document.getElementById('comprar3');
-    comprar3.addEventListener('click', function () {
-      const lista = document.getElementById('lista');
-      const nuevoElemento = document.createElement('li');
-      nuevoElemento.textContent = 'Social Media';
-      lista.appendChild(nuevoElemento);
-    });
-  });
-  localStorage.setItem("servicio", "Social Media");
-  
-  document.addEventListener('DOMContentLoaded', function () {
-     const comprar4 = document.getElementById('comprar4');
-    comprar4.addEventListener('click', function () {
-       const lista = document.getElementById('lista');
-       const nuevoElemento = document.createElement('li');
-   nuevoElemento.textContent = 'Web Development';
-    lista.appendChild(nuevoElemento);
-    });
-  });
-  localStorage.setItem("servicio", "Web Development");
+const servicios =[
+  {
+    id: "branding",
+    titulo: "Branding",
+    imagen: "./img/servicios/Branding.jpg",
+    precio: 6000
+  },
+  {
+    id: "publicidad",
+    titulo: "Publicidad",
+    imagen: "../img/servicios/Publicidad.jpg",
+    precio: 8000
+  },
+  {
+    id: "Social-Media",
+    titulo: "Social Media",
+    imagen: "./img/servicios/Social Media.jpg",
+    precio: 4500
+  },
+  {
+    id: "web-development",
+    titulo: "Web Development",
+    imagen: "../img/servicios/Web Development.jpg",
+    precio: 5000
+  },
+];
 
+const contenedorServicios = document.querySelector("#contenedor-servicios");
+let botonContratar = document.querySelectorAll(".btn-secondary");
+const indicador = document.querySelector("#indicador");
+
+
+function cargarServicios() {
+servicios.forEach(servicio => {
+
+  const div = document.createElement("div");
+  div.classList.add("contenedor-servicio");
+  div.innerHTML = `
+  <ul> <div class="contendedor-servicio">
+  <div class="row">
+  <img  class="card-img-top" src="${servicio.imagen}" alt="${servicio.titulo}">
+    <h2 class="card-title">${servicio.titulo}</h2>
+    <p class="card-text">Trabajaremos sobre la marca de tu negocio (re)construyéndola desde el principio.
+      Investigaremos el
+      segmento del mercado en el que te posicionas e identificaremos tu target de forma detallada.
+      </p>
+    <main class="text-end">
+      <a id= class="btn btn-secondary" >$${servicio.precio}</a>
+     </main>
+     <main class="text-end">
+       <button class="btn-secondary" id="${servicio.id}" >Contratar</button>
+   </main>
+  </div>
+  </ul>
+  `;
+  contenedorServicios.append(div);
+  actualizarBotonContratar();
+})
+
+}
+
+cargarServicios();
+
+function actualizarBotonContratar() {
+  botonContratar = document.querySelectorAll(".btn-secondary");
+  botonContratar.forEach(boton => {
+  boton.addEventListener("click", agregarAlCarrito);
+  });
+} 
+
+const serviciosEnCarrito = [];
+function agregarAlCarrito(e) {
+const idBoton = e.currentTarget.id;
+const servicioAgregado = servicios.find(servicio => servicio.id === idBoton);
+
+;
+if(serviciosEnCarrito.some(servicio => servicio.id === idBoton)){
+  const index = serviciosEnCarrito.findIndex(servicio => servicio.id === idBoton);
+  serviciosEnCarrito[index].cantidad++;
+} else{
+  servicioAgregado.cantidad = 1;
+  serviciosEnCarrito.push(servicioAgregado);
+}
+actualizarIndicador();
+
+localStorage.setItem("servicios-en-carrito", JSON.stringify(serviciosEnCarrito));
+}
+function actualizarIndicador () {
+  let nuevoIndicador = serviciosEnCarrito.reduce((acc, servicio) => acc + servicio.cantidad, 0);
+  indicaro 
+}
